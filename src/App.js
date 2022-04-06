916e9929e3a531b88632b372306076db13bb35b4
krishna yele <krishnayele@gmail.com>
2022-04-07 01:28:56 +0530

    adding build

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h3>Hello Krishna</h3>
         <a
           className="App-link"
           href="https://reactjs.org"
