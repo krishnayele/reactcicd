a593f931eb9be2db7665845398c5a3c236187ad9
krishna yele <krishnayele@gmail.com>
2022-04-07 09:12:54 +0530

    included icon package

--- src/Components/Appbar/appbarcomponent.js
+++ src/Components/Appbar/appbarcomponent.js
@@ -4,7 +4,7 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
+import { Menu } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
 import Avatar from '@mui/material/Avatar';
