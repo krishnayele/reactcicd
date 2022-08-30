f2197f5563658f4f9f0ec26db3550ff364f6d5b1
Krishna Yele <krishnayele@gmail.com>
2022-08-30 17:44:47 +0530

    Update Mydetailspage.js
    added a comment to check if Pipeline works

--- src/Components/IntroductionSection/Mydetailspage.js
+++ src/Components/IntroductionSection/Mydetailspage.js
@@ -13,6 +13,7 @@ function Mydetailspage() {
         <Typography alignContent={"center"} variant="h2">Hi! </Typography>
         <Typography typeof={""} variant="h2"> Krishna Yele Here</Typography>
         <Typography variant="h3">A cloud Consultant</Typography>
+        <div>Checking if the App works</div>
         </Paper>
 
   )
